c788b063a496b9a8f891cc2993d7c3fd592939db
Sergii Turivnyi <sturivnyi@mirantis.com>
2018-10-31 17:49:09 +0200

    Add Update tests feature

--- get_info.py
+++ get_info.py
@@ -58,19 +58,79 @@ class GetFailedTests:
         return all_tests
 
     def get_failed_tests(self):
-        # test['status_id'] == 5 failed test
+        # test['status_id'] == 5 failed
         # test['status_id'] == 9 test failed
+        # test['status_id'] == 10 infra failed
         all_tests_in_all_suites = self.get_all_tests_results()
 
         failed_tests = []
         for tests_in_suite in all_tests_in_all_suites:
             for test in tests_in_suite:
-                if test['status_id'] == 9:
+                if test['status_id'] == 5:
                     failed_tests.append(test)
 
         return failed_tests
 
+    def get_test(self, test_id):
+        return client.send_get('get_test/{}'.format(test_id))
+
+    def get_test_result(self, test_id):
+        test = self.get_test(test_id)
+        return client.send_get('get_results_for_case/{}/{}'.format(test['run_id'], test['case_id']))
+
+    def generate_link_on_failed_test(self, tests_ids):
+        all_tests = []
+        for t_id in tests_ids:
+            all_tests.append('https://mirantis.testrail.com/index.php?/tests/view/{}'.format(t_id))
+
+        for test in all_tests:
+            print(test)
+        return all_tests
+
+    def get_failed_tests_results(self):
+        failed_tests = self.get_failed_tests()
+        test_results = []
+        for test in failed_tests:
+            test_results.append(
+                client.send_get('get_results_for_case/{}/{}'.format(test['run_id'], test['case_id'])))
+        return test_results
+
+    def get_failed_infra_tests(self):
+        failed_tests = self.get_failed_tests()
+        # https://mirantis.jira.com/wiki/spaces/OSCORE/pages/1190856164/DevCloud+Infra+Issues
+        INFRA_FAILED_TESTS = ['id-cc54ca6e-b91d-4ddd-80cc-24a886dfaaa0',
+                              'id-3f7726fc-a41b-40ca-ab38-51e2973f146a',
+                              'id-8da0f6cc-60e6-4298-9e54-e1f905c5552a',
+                              'id-3d753d42-7c16-4a0e-8f73-875881826626']
+        test_for_update = []
+        for i_test in INFRA_FAILED_TESTS:
+            for test in failed_tests:
+                if i_test in test['title']:
+                    test_for_update.append(test)
+                    print('https://mirantis.testrail.com/index.php?/tests/view/{}'.format(test['id']))
+        return test_for_update
+
+    def update_failed_infra_tests(self):
+        tests_for_update = self.get_failed_infra_tests()
+
+        # test['status_id'] == 10 infra failed
+        add_result = {
+            'status_id': '10',
+            'defects': 'PROD-22993',
+            'comment': None,
+            'elapsed': None,
+            'version': None,
+            'assignedto_id': None, }
+
+        updated_test_results = []
+        print("============= update_result =============")
+        for test in tests_for_update:
+            update_result = client.send_post('add_result/{}'.format(test['id']), add_result)
+            updated_test_results.append(update_result)
+            print(update_result['title'])
+        return updated_test_results
+
 
 if __name__ == '__main__':
-    tests = GetFailedTests('51082')
-    print(len(tests.get_failed_tests()))
+    tests = GetFailedTests('51312')
+    print(len(tests.get_failed_tests_results()))
